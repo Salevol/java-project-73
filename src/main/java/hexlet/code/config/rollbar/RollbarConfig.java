e63dd63a74383edcdf27c81d8835d562da8a5928
asankovsky <asankovsky@gmail.com>
2023-06-12 20:56:47 +0300

    feat: rollbar

---
+++ src/main/java/hexlet/code/config/rollbar/RollbarConfig.java
@@ -0,0 +1,31 @@
+package hexlet.code.config.rollbar;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import com.rollbar.notifier.Rollbar;
+import com.rollbar.notifier.config.Config;
+import com.rollbar.spring.webmvc.RollbarSpringConfigBuilder;
+
+@Configuration()
+@ComponentScan({
+        "hexlet.code.app"
+})
+public class RollbarConfig {
+
+    @Value("${rollbar_token:}")
+    private String rollbarToken;
+
+    @Bean
+    public Rollbar rollbar() {
+        return new Rollbar(getRollbarConfigs(rollbarToken));
+    }
+
+    private Config getRollbarConfigs(String accessToken) {
+
+        return RollbarSpringConfigBuilder.withAccessToken(accessToken)
+                .environment("development")
+                .build();
+    }
+}
